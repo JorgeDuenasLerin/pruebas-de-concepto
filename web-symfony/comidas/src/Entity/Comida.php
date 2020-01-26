4c38e84c24b9b8f2383034e19d0a2202b21f9e17
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2020-01-26 11:24:38 +0100

    Proyecto Comidas Symfony

---
+++ web-symfony/comidas/src/Entity/Comida.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ComidaRepository")
+ */
+class Comida
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Categoria", inversedBy="comidas")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $categoria;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getCategoria(): ?Categoria
+    {
+        return $this->categoria;
+    }
+
+    public function setCategoria(?Categoria $categoria): self
+    {
+        $this->categoria = $categoria;
+
+        return $this;
+    }
+
+    public function __toString() {
+        return $this->nombre;
+    }
+}
